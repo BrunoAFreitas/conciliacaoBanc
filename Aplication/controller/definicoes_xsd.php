967764f1d7cd73f28a589efd6936efe203dee0b6
Bruno Araujo <bruno_araujo_294@hotmail.com>
2014-06-26 16:25:39 -0300

    Definicoes xsd

---
+++ Aplication/controller/definicoes_xsd.php
@@ -0,0 +1,34 @@
+<?php
+
+    $_metodo[buscaPrazoParcelasFlex] = "buscaPrazoParcelasFlexRequest";
+    $_metodo[consultaDisponibilidadeCanal] = "canalRequest";
+    $_metodo[consultaElegibilidadeFinanciamentoVeiculo] = "elegibilidadeRequest";
+    $_metodo[consultaStatusPropostasFinanciamentoVeiculo] = "statusPropostasRequest";
+    $_metodo[enviaPropostaFinanciamentoVeiculoPasso1Inicio] = "inserePropostaRequest";
+    $_metodo[enviaPropostaFinanciamentoVeiculoPasso2DadosPessoais] = "dadosPessoaisRequest";
+    $_metodo[enviaPropostaFinanciamentoVeiculoPasso3Endereco] = "insereEnderecoRequest";
+    $_metodo[enviaPropostaFinanciamentoVeiculoPasso4Referencias] = "arg0";
+    $_metodo[enviaPropostaFinanciamentoVeiculoPasso5Garantias] = "insereGarantiaRequest";
+    $_metodo[enviaPropostaFinanciamentoVeiculoPasso6Fim] = "dadosAdpRequest";
+    $_metodo[getDocument] = "request";
+    $_metodo[imprimeCETPDF] = "impressaoRequest";
+    $_metodo[imprimeDocumentosGerais] = "imprimeDocumentosGeraisRequest";
+    $_metodo[imprimeDocumentosGeraisMulti] = "imprimeDocumentosGeraisMultiRequest";
+    $_metodo[listaAtividadesEconomicas] = "dominioAtividadeEconomicaRequest";
+    $_metodo[listaDominiosGerais] = "dominiosGeraisRequest";
+    $_metodo[listaFormasPagamento] = "dominioFormaPagamentoRequest";
+    $_metodo[listaModalidade] = "listaModalidadeRequest";
+    $_metodo[listaVeiculoAnoCombustivel] = "dominioAnoCombustivelRequest";
+    $_metodo[listaVeiculoMarcas] = "dominioMarcaRequest";
+    $_metodo[listaVeiculoModelos] = "dominioModeloRequest";
+    $_metodo[listarBancos] = "dominioBancoRequest";
+    $_metodo[listarGarantias] = "dominioGarantiaRequest";
+    $_metodo[listarModalidadesOutrosBens] = "dominioModalidadeRequest";
+    $_metodo[listarTipoRelacionamento] = "tipoRelacionamentoClientRequest";
+    $_metodo[simulaFinanciamentoOutrosBens] = "simulacaoPropostaRequest";
+    $_metodo[simulaFinanciamentoVeiculo] = "simulacaoPropostaRequest";
+    $_metodo[validaGravame] = "request";
+    $_metodo[validaProfissao] = "request";
+    $_metodo[verificarSeguroPreSimulacao] = "request";
+
+?>
